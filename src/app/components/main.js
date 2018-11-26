0dd8895c6df04d0a5ae9bc8cf8fde4187f0084c3
murphym757 <murphym757@yahoo.com>
2018-11-26 00:05:59 -0500

    Completed initial styling, design, and development

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -14,25 +14,22 @@ export default class MainSource extends Component {
             generalInfo: {
 
             },
-            cityName: {
-                name: "Miami"
-            },
+            cityName: null,
             weatherIcon: ''
         };
     }
 
-    
-
-    componentDidMount() {
-        this.generalTempInfo();
-      }
+    cityChange = (e) =>{
+        this.setState({cityName: e.target.value});
+    }
 
       /* Test URL (Does Work)
       http://api.openweathermap.org/data/2.5/find?q=Miami&units=imperial&appid=ae0766143a31a2063be8be2843baf16c
       */
-    generalTempInfo() {
+    handleSubmit = (e) => {
+    e.preventDefault();
         const baseUrl = 'http://api.openweathermap.org/data/2.5/find?q=';
-        const City = this.state.cityName.name;
+        const City = this.state.cityName;
         const unitsApiID = '&units=imperial&appid=';
         const APIKEY = "ae0766143a31a2063be8be2843baf16c";
 
@@ -96,21 +93,91 @@ export default class MainSource extends Component {
     render() {
         const tempNow = Math.trunc(this.state.currentTemp.temp);
         return (
-            <div>
-                <p>I will display &#8457;</p>
-                <h1>{tempNow} &#8457;</h1>
-                <form>
-                    <input 
-                        /*type="text" 
-                        value={this.state.username} 
-                        onChange={this.loginUsername} 
-                        class="form-control form-souse" 
-                        name="username" 
-                        id="loginUsername" 
-                        placeholder="Username" */
-                    />
-                </form>
-                <i class={this.state.weatherIcon}></i>
+            <div class="container">
+                <div class="card">
+                <img class="logo mx-auto" src="./src/app/assets/images/Weather-or-Pop.svg" alt="Smiley face" height="90" width="90" />
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            type="text" 
+                            value={this.state.cityName} 
+                            onChange={this.cityChange} 
+                            class="form-control form-weatherOrPop" 
+                            name="City" 
+                            id="cityName" 
+                            placeholder="City"
+                        />
+                    </form>
+                    <div class="container-fluid">
+                        <div class="row"> {/*Top Row*/}
+                            <div class="card card1 col-6 rounded-0"> {/*Top Left Card*/}
+                                <h1 class="locationName-card1 pb-2">{this.state.locationInfo.location}</h1>
+                                <h1 class="currentTemp-card1">{tempNow} &#176;F</h1>
+                                <div class="row">
+                                    <h2 class="col-12 weatherPic-card1"><i class={this.state.weatherIcon}></i></h2>
+                                    <h3 class="col-12 weatherDescription-card1">{this.state.generalInfo.weatherDescription}</h3>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6 coords-card1">
+                                        {this.state.generalInfo.coordLat}
+                                    </div>
+                                    <div class="col-6 coords-card1">
+                                        {this.state.generalInfo.coordLon}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card2 col-6 rounded-0"> {/*Top Right Card*/}
+                                <h1 class="locationName-card2 pb-2">{this.state.locationInfo.location}</h1>
+                                <h1 class="currentTemp-card2">{tempNow} &#176;F</h1>
+                                <div class="row">
+                                    <h2 class="col-12 weatherPic-card2"><i class={this.state.weatherIcon}></i></h2>
+                                    <h3 class="col-12 weatherDescription-card2">{this.state.generalInfo.weatherDescription}</h3>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6 coords-card2">
+                                        {this.state.generalInfo.coordLat}
+                                    </div>
+                                    <div class="col-6 coords-card2">
+                                        {this.state.generalInfo.coordLon}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row"> {/*Bottom Row*/}
+                            <div class="card card3 col-6 rounded-0"> {/*Bottom Left Card*/}
+                                <h1 class="locationName-card3 pb-2">{this.state.locationInfo.location}</h1>
+                                <h1 class="currentTemp-card3">{tempNow} &#176;F</h1>
+                                <div class="row">
+                                    <h2 class="col-12 weatherPic-card3"><i class={this.state.weatherIcon}></i></h2>
+                                    <h3 class="col-12 weatherDescription-card3">{this.state.generalInfo.weatherDescription}</h3>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6 coords-card3">
+                                        {this.state.generalInfo.coordLat}
+                                    </div>
+                                    <div class="col-6 coords-card3">
+                                        {this.state.generalInfo.coordLon}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card4 col-6 rounded-0"> {/*Bottom Right Card*/}
+                                <h1 class="locationName-card4 pb-2">{this.state.locationInfo.location}</h1>
+                                <h1 class="currentTemp-card4">{tempNow} &#176;F</h1>
+                                <div class="row">
+                                    <h2 class="col-12 weatherPic-card4"><i class={this.state.weatherIcon}></i></h2>
+                                    <h3 class="col-12 weatherDescription-card4">{this.state.generalInfo.weatherDescription}</h3>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6 coords-card4">
+                                        {this.state.generalInfo.coordLat}
+                                    </div>
+                                    <div class="col-6 coords-card4">
+                                        {this.state.generalInfo.coordLon}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
           );
       }
