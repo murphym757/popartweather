4549d2f9469882a4e8c561a00400545912e1a261
murphym757 <murphym757@yahoo.com>
2018-11-28 20:28:20 -0500

    Added Mapbox API for coordinate searching and DarkSky for more robust forecast data

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -14,24 +14,68 @@ export default class MainSource extends Component {
             generalInfo: {
 
             },
-            cityName: null,
+            darkSky: {
+
+            },
+            mapBox: {
+
+            },
+            cityName: "Miami",
+            location: null,
+            locationLat: null,
+            locationLon: null,
             weatherIcon: ''
         };
     }
 
-    cityChange = (e) =>{
-        this.setState({cityName: e.target.value});
+    locationChange = (e) =>{
+        this.setState({location: e.target.value});
     }
+    latSet = (e) =>{
+        this.setState({locationLat: e.target.value});
+    }
+    lonSet = (e) =>{
+        this.setState({locationLon: e.target.value});
+    }
+
+    locationSubmit = (e) => {
+        e.preventDefault();
+        //Map Box API
+        const mapBoxBaseUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+        const mapBoxlocation = this.state.location;
+        const mapBoxParseInfo = ".json?access_token=";
+        const mapBoxAPI = "pk.eyJ1IjoibXVycGh5bTc1NyIsImEiOiJjanAwbXhxZ3Ewa2ZqM3dvOTA1eHFseXU1In0.K2jhpceCSdXnnczqm65_BQ";
+        axios.get(mapBoxBaseUrl + mapBoxlocation + mapBoxParseInfo + mapBoxAPI)
+            .then(res => {
+                this.setState({
+                    mapBox: {
+                        location: res.data.features[0].place_name,
+                        latitude: res.data.features[0].center[1],
+                        longitude: res.data.features[0].center[0]
+                    }
+                });
+            })
+    }
+
 
       /* Test URL (Does Work)
       http://api.openweathermap.org/data/2.5/find?q=Miami&units=imperial&appid=ae0766143a31a2063be8be2843baf16c
       */
     handleSubmit = (e) => {
     e.preventDefault();
+        // OpenWeather API
         const baseUrl = 'http://api.openweathermap.org/data/2.5/find?q=';
         const City = this.state.cityName;
         const unitsApiID = '&units=imperial&appid=';
         const APIKEY = "ae0766143a31a2063be8be2843baf16c";
+        
+        // Dark Sky
+        const darkSkyProxy = "https://cors-anywhere.herokuapp.com/";
+        const darkSkyBaseUrl = "https://api.darksky.net/forecast/"
+        const darkSkyAPI = "52c9fce56df8103a35f1f162841fa65e";
+        const darkSkyLat = this.state.locationLat;
+        const darkSkyLon = this.state.locationLon;
+        const darkSkyCoordinates = darkSkyLat + "," + darkSkyLon;
 
         //Weather Icons
         const cloudyPic = "fas fa-cloud";
@@ -87,7 +131,15 @@ export default class MainSource extends Component {
                     }
                 });
             })
-}
+            axios.get(darkSkyProxy + darkSkyBaseUrl + darkSkyAPI +'/' + darkSkyCoordinates)
+                .then(res => {
+                    this.setState({
+                        darkSky: {
+                            temp: res.data.currently.temperature
+                        }
+                    });
+                })
+        }
 
 
     render() {
@@ -96,21 +148,50 @@ export default class MainSource extends Component {
             <div class="container">
                 <div class="card">
                 <img class="logo mx-auto" src="./src/app/assets/images/Weather-or-Pop.svg" alt="Smiley face" height="90" width="90" />
-                    <form onSubmit={this.handleSubmit}>
+                    <form class="form-group" onSubmit={this.locationSubmit.bind(this)}>
                         <input 
                             type="text" 
-                            value={this.state.cityName} 
-                            onChange={this.cityChange} 
+                            value={this.state.location} 
+                            onChange={this.locationChange} 
                             class="form-control form-weatherOrPop" 
-                            name="City" 
-                            id="cityName" 
-                            placeholder="City"
+                            name="Location" 
+                            id="locationName" 
+                            placeholder="Location"
                         />
+                        <label>
+                            Coordinates: {this.state.mapBox.latitude + ", " + this.state.mapBox.longitude}
+                        </label>
+                    </form>
+                    <form class="form-group" onSubmit={this.handleSubmit}>
+                        <input 
+                            type="text" 
+                            value={this.state.locationLat} 
+                            onChange={this.latSet} 
+                            class="form-control form-weatherOrPop" 
+                            name="Latitude" 
+                            id="latitude" 
+                            placeholder="Latitude"
+                        />
+                        <input 
+                            type="text" 
+                            value={this.state.locationLon} 
+                            onChange={this.lonSet} 
+                            class="form-control form-weatherOrPop" 
+                            name="Longitude" 
+                            id="longitude" 
+                            placeholder="Longitude"
+                        />
+                        <button 
+                            type="button"
+                            onClick = {this.handleSubmit.bind(this)}
+                            class="btn btn-souse-extra btn-lg btn-block extra-button-font">
+                                FIND WEATHER
+                        </button>
                     </form>
                     <div class="container-fluid">
                         <div class="row"> {/*Top Row*/}
                             <div class="card card1 col-6 rounded-0"> {/*Top Left Card*/}
-                                <h1 class="locationName-card1 pb-2">{this.state.locationInfo.location}</h1>
+                                <h1 class="locationName-card1 pb-2">{this.state.darkSky.temp}</h1>
                                 <h1 class="currentTemp-card1">{tempNow} &#176;F</h1>
                                 <div class="row">
                                     <h2 class="col-12 weatherPic-card1"><i class={this.state.weatherIcon}></i></h2>
